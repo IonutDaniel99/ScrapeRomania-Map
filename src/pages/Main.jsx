e4fc9d57518f9e005024dc94cdf64d43db2ce16e
Ionut Daniel Ene <IonutDaniel.Ene@endava.com>
2022-06-24 15:50:01 +0300

    paralax implement

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Maps from '../components/Maps/Maps';
 import NavBar from '../components/NavBar/NavBar';
+import {MouseParallaxChild, MouseParallaxContainer} from 'react-parallax-mouse';
 
 function Main() {
 
-    window.addEventListener("touchstart", touchHandler, { passive: false });
+    window.addEventListener('touchstart', touchHandler, { passive: false });
     
     function touchHandler(event) {
         if (event.touches.length > 1) {
@@ -13,13 +14,28 @@ function Main() {
     }
 
     return (
-        <div className='h-screen flex justify-center items-center flex-col bg-white overflow-hidden'>
-            <img className={'absolute top-0 left-0 w-full h-full flex justify-center items-center z-0'}
-                 src={'https://images.pexels.com/photos/326333/pexels-photo-326333.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}
-                 alt={'cat'}/>
-            <Maps />
-            <NavBar />
-        </div>
+        <>
+            <MouseParallaxContainer
+                className="parallax w-screen h-screen "
+                containerStyles={{
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%'
+                }}
+                resetOnLeave
+            >
+                <MouseParallaxChild
+                    factorX={0.05}
+                    factorY={0.05}
+                >
+                    <img width={'100%'} height={'100%'} draggable="false" className={'scale-105'} src="https://images.pexels.com/photos/326333/pexels-photo-326333.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                </MouseParallaxChild>
+            </MouseParallaxContainer>
+            <div className='h-screen w-screen flex justify-center items-center flex-col bg-white overflow-hidden'>
+                <Maps />
+                <NavBar />
+            </div>
+        </>
     );
 }
 
